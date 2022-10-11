62a2f603ab9a3dad18a3e18800eaaf395b942f0c
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2022-10-11 10:12:32 +0200

    Project Added

---
+++ Source/UnrealVFX/UnrealVFX.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealVFX.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealVFX, "UnrealVFX" );
---
+++ Source/UnrealVFX/UnrealVFXGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealVFXGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALVFX_API AUnrealVFXGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
